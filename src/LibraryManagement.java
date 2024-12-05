263951afe7ec851b991325408390a91867e245e9
DelroyCCampbell <cdelroy86@gmail.com>
2024-12-04 21:40:44 -0500

    Update LibraryManagement.java
    Updated main method to handle exception

--- src/LibraryManagement.java
+++ src/LibraryManagement.java
@@ -1,115 +1,136 @@
 import java.util.Scanner;
 
-
-
 public class LibraryManagement {
-
     private Library library = new Library();
 
-
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
         new LibraryManagement().run();
-
     }
 
-
-
-    private void run() {
-
+    private void run() throws Exception {
         Scanner scanner = new Scanner(System.in);
-
         boolean running = true;
 
-
-
         while (running) {
-
             System.out.println("===========================");
-
             System.out.println("Library Management System");
-
             System.out.println("1. Add Member");
-
             System.out.println("2. Add Book");
-
             System.out.println("3. Borrow Book");
-
             System.out.println("4. Return Book");
-
             System.out.println("5. View Borrowed Books");
-
             System.out.println("6. View Transaction History");
-
             System.out.println("7. Exit");
-
             System.out.println("===========================");
-
             System.out.print("Enter your choice: ");
 
-
-
             int choice = scanner.nextInt();
-
             scanner.nextLine();
 
-
-
             switch (choice) {
-
                 case 1:
+                    System.out.print("Enter member ID: ");
+                    int id = scanner.nextInt();
+                	System.out.print("Enter member name: ");
+                    String name = scanner.next();
+                    
+                    scanner.nextLine();
 
-                    addMember(scanner);
-
+                    Member newMember = new Member(id, name);
+                    library.addMember(newMember);
+                    System.out.println("Member added successfully.");
                     break;
-
                 case 2:
+                    System.out.print("Enter book ID: ");
+                    id = scanner.nextInt();
+                    System.out.print("Enter book title: ");
+                    name = scanner.next();
 
-                    addBook(scanner);
-
+                    try {
+                        // Attempt to create and add a new book
+                        Book newBook = new Book(id, name);
+                        library.addBook(newBook);
+                        System.out.println("Book added to library successfully.");
+                    } catch (Exception e) {
+                        // Handle invalid ID error
+                        System.out.println("Error: " + e.getMessage());
+                    }
                     break;
-
                 case 3:
+                	System.out.println("\n--- Available Members ---");
+                    for (Member member : library.getMembers()) {
+                        System.out.println(member.getId() + ". " + member.getName());
+                    }
+                    
+                    System.out.print("Enter member ID: ");
+                    int memberId = scanner.nextInt();
+                    
+                    System.out.println("\n--- Available Books ---");
+                    for (Book book : library.getBooks()) {
+                        if (book.isAvailable())
+                            System.out.println(book.getId() + ". " + book.getTitle());
+                    }
+                    
+                    System.out.print("Enter book ID: ");
+                    int bookId = scanner.nextInt();
+                    
+                    scanner.nextLine();
 
-                    borrowBook(scanner);
+                    Member member = library.findMemberById(memberId);
+                    Book book = library.findBookById(bookId);
 
+                    if (member != null && book != null) {
+                    	Transaction transaction = new Transaction();
+						transaction.borrowBook(book, member);
+                    } else {
+                        System.out.println("Invalid member or book ID.");
+                    }
                     break;
-
                 case 4:
+                	System.out.print("Enter member ID: ");
+                    memberId = scanner.nextInt();
+                    
+                    System.out.print("Enter book ID: ");
+                    bookId = scanner.nextInt();
+                    
+                    scanner.nextLine();
 
-                    returnBook(scanner);
+                    member = library.findMemberById(memberId);
+                    book = library.findBookById(bookId);
 
+                    if (member != null && book != null) {
+                    	Transaction.returnBook(book, member);
+                    } else {
+                        System.out.println("Invalid member or book ID.");
+                    }
                     break;
-
                 case 5:
+                	System.out.print("Enter member ID: ");
+                    memberId = scanner.nextInt();
+                    scanner.nextLine();
 
-                    viewBorrowedBooks(scanner);
-
+                    Member specificMember = library.findMemberById(memberId);
+                    
+                    if (specificMember != null) {
+                        System.out.println("Books borrowed by " + specificMember.getName() + ":");
+                        for (Book bk : specificMember.getBorrowedBooks()) {
+                            System.out.println(" - " + bk.getTitle());
+                        }
+                    } else {
+                        System.out.println("Invalid member ID.");
+                    }
                     break;
-
                 case 6:
-
-                    viewTransactionHistory();
-
+                	Transaction.getTransaction().displayTransactionHistory();
                     break;
-
                 case 7:
-
                     System.out.println("Exiting. Good Bye..");
-
                     running = false;
-
                     break;
-
                 default:
-
                     System.out.println("Invalid choice! Please try again.");
-
             }
-
         }
-
     }
 
 
@@ -143,7 +164,7 @@ public class LibraryManagement {
 
 
 
-    private void addBook(Scanner scanner) {
+    private void addBook(Scanner scanner) throws Exception {
 
         System.out.print("Enter the book ID number: ");
 
